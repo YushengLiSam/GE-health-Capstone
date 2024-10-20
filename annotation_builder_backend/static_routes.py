26e0f7ea0a0b4dde1392ee10daa1b5c8e3ebf93f
dianakotsonis <159602024+dianakotsonis@users.noreply.github.com>
2024-10-20 14:15:33 -0500

    removed unused file

--- annotation_builder_backend/static_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, request
-import mysql.connector
-
-# Connect to the static database
-db = mysql.connector.connect(
-    host="localhost",
-    user="annotation_user",
-    password="",
-    database="",
