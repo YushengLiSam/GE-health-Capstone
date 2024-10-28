a9faf9b9ebcb518d8112bc6aeadb79c019b234c6
Yusheng Li <li2339@wisc.edu>
2024-10-27 19:49:43 -0500

    Update database structure and routes

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,10 @@
 from flask import Flask
-from api_routes import category_routes, subcategory_routes, datapoint_routes, operand_routes
+from api_routes import api_routes
 
 app = Flask(__name__)
 
 # 注册蓝图
-app.register_blueprint(category_routes, url_prefix='/categories')
-app.register_blueprint(subcategory_routes, url_prefix='/subcategories')
-app.register_blueprint(datapoint_routes, url_prefix='/datapoints')
-app.register_blueprint(operand_routes, url_prefix='/operands')
-
-
-@app.route('/')
-def home():
-    return "Backend is connected to MySQL!"
-
+app.register_blueprint(api_routes, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
