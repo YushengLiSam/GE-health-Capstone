74c09eab11fda5897599abe4ca6e6b5b33ea282f
WEIQI ZHOU <wzhou255@wisc.edu>
2024-10-28 16:34:49 -0500

    style refine

--- ge-health/src/Components/UserInterface/forms/PatientCareForm.jsx
+++ ge-health/src/Components/UserInterface/forms/PatientCareForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import './form.css';
 
 function PatientCareForm() {
   const [formData, setFormData] = useState({
@@ -34,13 +35,14 @@ function PatientCareForm() {
   return (
     <Form onSubmit={handleSubmit}>
       {Object.keys(formData).map((key) => (
-        <Form.Group className="mb-3" key={key} controlId={key}>
+        <Form.Group className="mb-3 input-group" key={key} controlId={key}>
           <Form.Label>{key.replace(/([A-Z])/g, ' $1')}</Form.Label>
           <Form.Control
             type="text"
             name={key}
             value={formData[key]}
             onChange={handleChange}
+            className="input-field"
           />
         </Form.Group>
       ))}
