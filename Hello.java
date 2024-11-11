babc77e9132a2c75f6083d705095ae0432f0da99
WEIQI ZHOU <wzhou255@wisc.edu>
2024-11-11 00:33:01 -0600

    delete test file

--- Hello.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("");
-    System.out.println("This is my contribution.");
-  }
-}
