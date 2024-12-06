ca1a59248be11daf4fc918fc6859b5c60666fc56
cdai532023 <dariodai0128@outlook.com>
2024-12-05 19:49:15 -0600

    Modularize chart.

--- ge-health/src/Components/builder/Sidebar.jsx
+++ ge-health/src/Components/builder/Sidebar.jsx
@@ -8,6 +8,12 @@ function Sidebar() {
     <div className="sidebar">
       <h3>Labor & Delivery</h3>
       <nav>
+        <NavLink
+          to="/admin/annotation-list"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          Annotation List
+        </NavLink>
         <NavLink to="/admin/rule-builder" className={({ isActive }) => (isActive ? 'active' : '')}>
           Rule Builder
         </NavLink>
